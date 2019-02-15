e021863d9978c385b688ed666ab19b77f36e8924
Ahmed Al-Ramadhan <47664868+Ahmadajr1@users.noreply.github.com>
2019-02-15 16:51:05 +0300

    Update PhotoAlbumCollectionViewController.swift
    feat: Informing the user that a photo has been successfully saved 
    
    function "ShowToast" was added to to inform the user that a photo has been successfully saved by showing a toast label that disappears in a 2 seconds duration.
    
    Without this feature, the user has no clue whether the photo is saved or not.

--- Virtual Tourist/Virtual Tourist/PhotoAlbumCollectionViewController.swift
+++ Virtual Tourist/Virtual Tourist/PhotoAlbumCollectionViewController.swift
@@ -137,11 +137,29 @@ class PhotoAlbumCollectionViewController: FlickrApi,UICollectionViewDataSource,U
                     }
                     else{
                         self.dismiss(animated: true, completion: nil)
+                        self.showToast(message: "Saved!")
                     }
                 }
                 present(activityVC, animated: true)
             }
         }
+    }
+    
+    func showToast(message : String) {
+        
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-300, width: 150, height: 50))
+        toastLabel.backgroundColor = UIColor.white.withAlphaComponent(0.9)
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 2.0, delay: 0.1, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
     }
     
     func searchByLatLon(){
